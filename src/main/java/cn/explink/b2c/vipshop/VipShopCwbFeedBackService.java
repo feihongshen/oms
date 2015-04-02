8ad619aeb218def14b2317c74fb2cd2a853dbacc
zpk <zpk@pjbest.com>
2015-04-02 10:42:13 +0000

    【修改】修改唯品会开箱验货的需求 张鹏凯

--- src/main/java/cn/explink/b2c/vipshop/VipShopCwbFeedBackService.java
+++ src/main/java/cn/explink/b2c/vipshop/VipShopCwbFeedBackService.java
@@ -500,7 +500,7 @@ public class VipShopCwbFeedBackService {
 		xml = xml.replaceAll("<trace>", "").replaceAll("</trace>", "").replaceAll("<cust_data_id>", "").replaceAll("</cust_data_id>", "").replaceAll("<order_sn>", "").replaceAll("</order_sn>", "")
 				.replaceAll("<order_status>", "").replaceAll("</order_status>", "").replaceAll("<order_status_info>", "").replaceAll("</order_status_info>", "").replaceAll("<current_city_name>", "")
 				.replaceAll("</current_city_name>", "").replaceAll("<order_status_time>", "").replaceAll("</order_status_time>", "").replaceAll("<sign_man>", "").replaceAll("</sign_man>", "")
-				.replaceAll("<is_unpacked>", "").replaceAll("</is_unpacked>", "")
+				.replaceAll("<is_unpacked></is_unpacked>", "").replaceAll("<is_unpacked>0</is_unpacked>", "").replaceAll("<is_unpacked>1</is_unpacked>", "")
 			
 		// .replaceAll("<deliver_name>","").replaceAll("</deliver_name>","")
 		// //不参与签名
