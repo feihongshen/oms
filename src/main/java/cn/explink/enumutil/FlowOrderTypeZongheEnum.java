16022c9f387a1a12570f10450b66fc8f4aa31621
gaoll <gaoll@localhost>
2015-07-02 06:37:10 +0000

    【修改】oms 枚举调整【高亮亮】

--- src/main/java/cn/explink/enumutil/FlowOrderTypeZongheEnum.java
+++ src/main/java/cn/explink/enumutil/FlowOrderTypeZongheEnum.java
@@ -3,13 +3,27 @@ package cn.explink.enumutil;
 public enum FlowOrderTypeZongheEnum {
 	DaoRuShuJu(1, "导入数据", "emaildatetime"),
 	// YouHuoWuDanDaoRuShuJu(2,"有货无单导入数据","NoListImportCwb"),
-	TiHuo(2, "提货", "getgoodstime"), RuKu(4, "入库", "intowarehoustime"), ChuKuSaoMiao(6, "出库", "outwarehousetime"), KuDuiKuChuKuSaoMiao(46, "库对库出库", "housetohousetime"), FenZhanDaoHuoSaoMiao(7, "分站到货",
-			"substationgoodstime"), FenZhanDaoHuoYouHuoWuDanSaoMiao(8, "到错货", "substationgoodstime"), FenZhanLingHuo(9, "领货", "receivegoodstime"), ZhongZhuanChuKuSaoMiao(10, "站点中转出站",
-			"zhandianouttozhongzhuantime"), ZhongZhuanZhanRuKu(12, "中转站入库", "changeintowarehoustime"), ZhongZhuanZhanChuKuSaoMiao(14, "中转站出库", "changeouttowarehoustime"), TuiHuoChuKuSaoMiao(40,
-			"站点退货出站", "returngoodsoutwarehousetime"), TuiHuoZhanRuKu(15, "退货站入库", "tuihuointowarehoustime"), TuiHuoZhanZaiTouSaoMiao(17, "退货站再投出库", "tuihuoouttozhandiantime"), TuiGongYingShangChuKu(
-			27, "退供货商出库", "customerbacktime"), GongYingShangJuShouFanKu(28, "供货商拒收返库", "gonghuoshangjushoutime"), GongHuoShangTuiHuoChenggong(34, "供货商退货成功", "gonghuoshangchenggongtime"), YiFanKui(35,
-			"反馈", "couplebacktime"), YiShenHe(36, "审核", "checktime"), YiChangDingDanChuLi(43, "异常订单处理", "yichangchuli"), DingDanLanJie(44, "订单拦截", "DingDanLanJie"), ShenHeWeiZaiTou(45, "审核为退货再投",
-			"ShenHeWeiZaiTou");
+	TiHuo(2, "提货", "getgoodstime"), 
+	RuKu(4, "入库", "intowarehoustime"), 
+	ChuKuSaoMiao(6, "出库", "outwarehousetime"), 
+	FenZhanDaoHuoSaoMiao(7, "分站到货","substationgoodstime"), 
+	FenZhanDaoHuoYouHuoWuDanSaoMiao(8, "到错货", "substationgoodstime"), 
+	FenZhanLingHuo(9, "领货", "receivegoodstime"), 
+	ZhongZhuanChuKuSaoMiao(10, "站点中转出站","zhandianouttozhongzhuantime"), 
+	ZhongZhuanZhanRuKu(12, "中转站入库", "changeintowarehoustime"), 
+	ZhongZhuanZhanChuKuSaoMiao(14, "中转站出库", "changeouttowarehoustime"), 
+	TuiHuoZhanRuKu(15, "退货站入库", "tuihuointowarehoustime"), 
+	TuiHuoZhanZaiTouSaoMiao(17, "退货站再投出库", "tuihuoouttozhandiantime"), 
+	TuiGongYingShangChuKu(27, "退供货商出库", "customerbacktime"), 
+	GongYingShangJuShouFanKu(28, "供货商拒收返库", "gonghuoshangjushoutime"), 
+	GongHuoShangTuiHuoChenggong(34, "供货商退货成功", "gonghuoshangchenggongtime"), 
+	YiFanKui(35,"反馈", "couplebacktime"), 
+	YiShenHe(36, "审核", "checktime"), 
+	TuiHuoChuKuSaoMiao(40,"站点退货出站", "returngoodsoutwarehousetime"), 
+	YiChangDingDanChuLi(43, "异常订单处理", "yichangchuli"), 
+	DingDanLanJie(44, "订单拦截", "DingDanLanJie"), 
+	ShenHeWeiZaiTou(45, "审核为退货再投","ShenHeWeiZaiTou"),
+	KuDuiKuChuKuSaoMiao(46, "库对库出库", "housetohousetime"); 
 
 	private int value;
 	private String text;
