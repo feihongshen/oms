837aa2bbb29fdcaf1b740b4ad740e784ccfed977
lans <lans@pjbest.com>
2015-12-27 05:01:30 +0000

    【修改】暂时提交打包不报错问题 【蓝生】

--- src/main/java/cn/explink/b2c/telecomsc/TelecomshopService.java
+++ src/main/java/cn/explink/b2c/telecomsc/TelecomshopService.java
@@ -131,7 +131,7 @@ public class TelecomshopService {
 		try {
 			String time = DateTimeUtil.getDateBefore(2);
 
-			b2cDataDAO.updateKeyWordByVipShop(customerids, time, null);
+			b2cDataDAO.updateKeyWordByVipShop(customerids, time,"", null);
 
 		} catch (Exception e) {
 			logger.error("重发电信商城异常", e);
