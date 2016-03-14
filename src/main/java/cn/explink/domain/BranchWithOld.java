f1c3abd6618d2807ba9bcfa48a3784901342b3c7
jianrong.gao <jianrong.gao>
2016-03-14 16:59:43 +0800

    修复bug:修改机构编码时，新机构编码不能同步到品骏达

---
+++ src/main/java/cn/explink/domain/BranchWithOld.java
@@ -0,0 +1,15 @@
+package cn.explink.domain;
+
+public class BranchWithOld extends Branch{
+	private String oldtpsbranchcode;//修改机构编码时旧的机构编码
+
+	public String getOldtpsbranchcode() {
+		return oldtpsbranchcode;
+	}
+
+	public void setOldtpsbranchcode(String oldtpsbranchcode) {
+		this.oldtpsbranchcode = oldtpsbranchcode;
+	}
+	
+	
+}
