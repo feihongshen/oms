660ea249a08c26725f0907b6a7024c1cf1f17638
jeef.fang <jeef.fang@vipshop.com>
2016-04-25 18:36:35 +0800

    plcs #none oms读写分离配置

---
+++ src/main/java/com/pjbest/splitting/routing/RoutingDataSource.java
@@ -0,0 +1,24 @@
+package com.pjbest.splitting.routing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+/**
+ * 
+ * @author pengfei.wan
+ * @date 2016-03-15
+ * 
+ */
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+	private static final Logger logger = LoggerFactory.getLogger(RoutingDataSource.class);
+
+	@Override
+	protected Object determineCurrentLookupKey() {
+		logger.info(">>> RoutingDataSource determineCurrentLookupKey thread : {}", Thread.currentThread().getName());
+		logger.info(">>> RoutingDataSource : {}", DatabaseContextHolder.peekDatabaseType());
+		return DatabaseContextHolder.peekDatabaseType();
+	}
+
+}
